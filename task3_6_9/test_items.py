0ad18c698fc053455412bac79244a92bee567ed8
EnikeevAI <54994879+EnikeevAI@users.noreply.github.com>
2020-01-11 17:30:15 +0300

    Update test_items.py
    add sleep timer for check task3_6_9

--- task3_6_9/test_items.py
+++ task3_6_9/test_items.py
@@ -4,7 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(10)
+    #time.sleep(30)
 
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert button, "Button add to basket not found"
