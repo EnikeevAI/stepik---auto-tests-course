f7cd5cf8fa9feae56d4c3c4318d69dbbc3649e07
eai <alexander.enikeev@gmail.com>
2020-01-11 17:21:47 +0300

    add files to check task3_6_9

---
+++ task3_6_9/test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, "Button add to basket not found"
